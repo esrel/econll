5631dc2192e9328f4f30f016b23e6b83ca85a7a7
Evgeny A. Stepanov <eas@vui.com>
2023-02-04 09:54:32 +0100

    updated setup

--- setup.py
+++ setup.py
@@ -1,14 +1,23 @@
-from setuptools import setup
+from setuptools import setup, find_packages
+
+import os
+
+
+def read(path):
+    return open(os.path.join(os.path.dirname(__file__), path)).read()
 
 
 setup(
     name='econll',
-    version='0.1.0',
-    description='',
-    long_description='',
+    version='0.1.2',
+    description='Extended CoNLL Sequence Labeling Evaluation Utilities',
+    readme="README.md",
+    long_description=read('README.md'),
+    long_description_content_type="text/markdown",
     url='https://github.com/esrel/econll',
     author='Evgeny A. Stepanov',
     author_email='stepanov.evgeny.a@gmail.com',
-    packages=[],
-    classifiers=['Development Status :: 1 - Planning'],
+    package_dir={'': "src"},
+    packages=find_packages('src'),
+    classifiers=['Development Status :: 2 - Pre-Alpha'],
 )

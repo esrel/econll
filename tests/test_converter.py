218ddac67c6d418cd07c4455fa53b46be2d9ff9d
Evgeny A. Stepanov <esrel@hotmail.com>
2024-01-11 11:59:45 +0100

    updated docstring

--- tests/test_converter.py
+++ tests/test_converter.py
@@ -83,8 +83,8 @@ def test_isa_parse() -> None:
     assert not ec.isa_parse([mdown])
 
 
-def test_convert_build_mdown() -> None:
-    """ test convert_mdown & build_mdown w/o synonyms """
+def test_from_make_mdown() -> None:
+    """ test from_mdown & make_mdown w/o synonyms """
     mdown = "[aaa](A) [bbb](B) [ccc](C) [ddd](D)"
     query = "aaa bbb ccc ddd"
     spans = [("A", 0, 3, "aaa"), ("B", 4, 7, "bbb"), ("C", 8, 11, "ccc"), ("D", 12, 15, "ddd")]
@@ -94,8 +94,8 @@ def test_convert_build_mdown() -> None:
     assert ec.make_mdown(*datas) == mdown
 
 
-def test_convert_build_mdown_syns() -> None:
-    """ test convert_mdown & build_mdown w/ synonyms """
+def test_from_make_mdown_syns() -> None:
+    """ test from_mdown & make_mdown w/ synonyms """
     mdown = "[aaa](A:A.0) [bbb](B:B.0) [ccc](C:C.0) [ddd](D:D.0)"
     query = "aaa bbb ccc ddd"
     spans = [("A", 0, 3, "A.0"), ("B", 4, 7, "B.0"), ("C", 8, 11, "C.0"), ("D", 12, 15, "D.0")]
@@ -105,8 +105,8 @@ def test_convert_build_mdown_syns() -> None:
     assert ec.make_mdown(*datas) == mdown
 
 
-def test_convert_build_conll() -> None:
-    """ test convert_conll & build_conll """
+def test_from_make_conll() -> None:
+    """ test from_conll & make_conll """
     conll = [("aaa", "B-A"), ("bbb", "B-B"), ("ccc", "B-C"), ("ddd", "B-D")]
     query = "aaa bbb ccc ddd"
     spans = [("A", 0, 3, "aaa"), ("B", 4, 7, "bbb"), ("C", 8, 11, "ccc"), ("D", 12, 15, "ddd")]
@@ -116,8 +116,8 @@ def test_convert_build_conll() -> None:
     assert ec.make_conll(*datas) == conll
 
 
-def test_convert_build_parse() -> None:
-    """ test convert_parse & build_parse """
+def test_from_make_parse() -> None:
+    """ test from_parse & make_parse """
     query = "aaa bbb ccc ddd"
     spans = [("A", 0, 3, "aaa"), ("B", 4, 7, "bbb"), ("C", 8, 11, "ccc"), ("D", 12, 15, "ddd")]
     datas = (query, "", spans)
